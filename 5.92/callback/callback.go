233892664a04acb0ca9b3fa00d7738bea006a0f6
Suvorov Daniil <severecloud@gmail.com>
2019-05-14 18:33:33 +0300

    feat(callback): funcList private

--- 5.92/callback/callback.go
+++ 5.92/callback/callback.go
@@ -15,7 +15,7 @@ type Callback struct {
 	ConfirmationKey  string
 	SecretKeys       map[int]string
 	SecretKey        string
-	FuncList         handler.FuncList
+	funcList         handler.FuncList
 }
 
 // HandleFunc handler
@@ -42,7 +42,7 @@ func (cb Callback) HandleFunc(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	cb.FuncList.Handler(e)
+	cb.funcList.Handler(e)
 	fmt.Fprintf(w, "ok")
 }
 
@@ -50,207 +50,207 @@ func (cb Callback) HandleFunc(w http.ResponseWriter, r *http.Request) {
 
 // MessageNew handler
 func (cb *Callback) MessageNew(f object.MessageNewFunc) {
-	cb.FuncList.MessageNew = append(cb.FuncList.MessageNew, f)
+	cb.funcList.MessageNew = append(cb.funcList.MessageNew, f)
 }
 
 // MessageReply handler
 func (cb *Callback) MessageReply(f object.MessageReplyFunc) {
-	cb.FuncList.MessageReply = append(cb.FuncList.MessageReply, f)
+	cb.funcList.MessageReply = append(cb.funcList.MessageReply, f)
 }
 
 // MessageEdit handler
 func (cb *Callback) MessageEdit(f object.MessageEditFunc) {
-	cb.FuncList.MessageEdit = append(cb.FuncList.MessageEdit, f)
+	cb.funcList.MessageEdit = append(cb.funcList.MessageEdit, f)
 }
 
 // MessageAllow handler
 func (cb *Callback) MessageAllow(f object.MessageAllowFunc) {
-	cb.FuncList.MessageAllow = append(cb.FuncList.MessageAllow, f)
+	cb.funcList.MessageAllow = append(cb.funcList.MessageAllow, f)
 }
 
 // MessageDeny handler
 func (cb *Callback) MessageDeny(f object.MessageDenyFunc) {
-	cb.FuncList.MessageDeny = append(cb.FuncList.MessageDeny, f)
+	cb.funcList.MessageDeny = append(cb.funcList.MessageDeny, f)
 }
 
 // MessageTypingState handler
 func (cb *Callback) MessageTypingState(f object.MessageTypingStateFunc) {
-	cb.FuncList.MessageTypingState = append(cb.FuncList.MessageTypingState, f)
+	cb.funcList.MessageTypingState = append(cb.funcList.MessageTypingState, f)
 }
 
 // PhotoNew handler
 func (cb *Callback) PhotoNew(f object.PhotoNewFunc) {
-	cb.FuncList.PhotoNew = append(cb.FuncList.PhotoNew, f)
+	cb.funcList.PhotoNew = append(cb.funcList.PhotoNew, f)
 }
 
 // PhotoCommentNew handler
 func (cb *Callback) PhotoCommentNew(f object.PhotoCommentNewFunc) {
-	cb.FuncList.PhotoCommentNew = append(cb.FuncList.PhotoCommentNew, f)
+	cb.funcList.PhotoCommentNew = append(cb.funcList.PhotoCommentNew, f)
 }
 
 // PhotoCommentEdit handler
 func (cb *Callback) PhotoCommentEdit(f object.PhotoCommentEditFunc) {
-	cb.FuncList.PhotoCommentEdit = append(cb.FuncList.PhotoCommentEdit, f)
+	cb.funcList.PhotoCommentEdit = append(cb.funcList.PhotoCommentEdit, f)
 }
 
 // PhotoCommentRestore handler
 func (cb *Callback) PhotoCommentRestore(f object.PhotoCommentRestoreFunc) {
-	cb.FuncList.PhotoCommentRestore = append(cb.FuncList.PhotoCommentRestore, f)
+	cb.funcList.PhotoCommentRestore = append(cb.funcList.PhotoCommentRestore, f)
 }
 
 // PhotoCommentDelete handler
 func (cb *Callback) PhotoCommentDelete(f object.PhotoCommentDeleteFunc) {
-	cb.FuncList.PhotoCommentDelete = append(cb.FuncList.PhotoCommentDelete, f)
+	cb.funcList.PhotoCommentDelete = append(cb.funcList.PhotoCommentDelete, f)
 }
 
 // AudioNew handler
 func (cb *Callback) AudioNew(f object.AudioNewFunc) {
-	cb.FuncList.AudioNew = append(cb.FuncList.AudioNew, f)
+	cb.funcList.AudioNew = append(cb.funcList.AudioNew, f)
 }
 
 // VideoNew handler
 func (cb *Callback) VideoNew(f object.VideoNewFunc) {
-	cb.FuncList.VideoNew = append(cb.FuncList.VideoNew, f)
+	cb.funcList.VideoNew = append(cb.funcList.VideoNew, f)
 }
 
 // VideoCommentNew handler
 func (cb *Callback) VideoCommentNew(f object.VideoCommentNewFunc) {
-	cb.FuncList.VideoCommentNew = append(cb.FuncList.VideoCommentNew, f)
+	cb.funcList.VideoCommentNew = append(cb.funcList.VideoCommentNew, f)
 }
 
 // VideoCommentEdit handler
 func (cb *Callback) VideoCommentEdit(f object.VideoCommentEditFunc) {
-	cb.FuncList.VideoCommentEdit = append(cb.FuncList.VideoCommentEdit, f)
+	cb.funcList.VideoCommentEdit = append(cb.funcList.VideoCommentEdit, f)
 }
 
 // VideoCommentRestore handler
 func (cb *Callback) VideoCommentRestore(f object.VideoCommentRestoreFunc) {
-	cb.FuncList.VideoCommentRestore = append(cb.FuncList.VideoCommentRestore, f)
+	cb.funcList.VideoCommentRestore = append(cb.funcList.VideoCommentRestore, f)
 }
 
 // VideoCommentDelete handler
 func (cb *Callback) VideoCommentDelete(f object.VideoCommentDeleteFunc) {
-	cb.FuncList.VideoCommentDelete = append(cb.FuncList.VideoCommentDelete, f)
+	cb.funcList.VideoCommentDelete = append(cb.funcList.VideoCommentDelete, f)
 }
 
 // WallPostNew handler
 func (cb *Callback) WallPostNew(f object.WallPostNewFunc) {
-	cb.FuncList.WallPostNew = append(cb.FuncList.WallPostNew, f)
+	cb.funcList.WallPostNew = append(cb.funcList.WallPostNew, f)
 }
 
 // WallRepost handler
 func (cb *Callback) WallRepost(f object.WallRepostFunc) {
-	cb.FuncList.WallRepost = append(cb.FuncList.WallRepost, f)
+	cb.funcList.WallRepost = append(cb.funcList.WallRepost, f)
 }
 
 // WallReplyNew handler
 func (cb *Callback) WallReplyNew(f object.WallReplyNewFunc) {
-	cb.FuncList.WallReplyNew = append(cb.FuncList.WallReplyNew, f)
+	cb.funcList.WallReplyNew = append(cb.funcList.WallReplyNew, f)
 }
 
 // WallReplyEdit handler
 func (cb *Callback) WallReplyEdit(f object.WallReplyEditFunc) {
-	cb.FuncList.WallReplyEdit = append(cb.FuncList.WallReplyEdit, f)
+	cb.funcList.WallReplyEdit = append(cb.funcList.WallReplyEdit, f)
 }
 
 // WallReplyRestore handler
 func (cb *Callback) WallReplyRestore(f object.WallReplyRestoreFunc) {
-	cb.FuncList.WallReplyRestore = append(cb.FuncList.WallReplyRestore, f)
+	cb.funcList.WallReplyRestore = append(cb.funcList.WallReplyRestore, f)
 }
 
 // WallReplyDelete handler
 func (cb *Callback) WallReplyDelete(f object.WallReplyDeleteFunc) {
-	cb.FuncList.WallReplyDelete = append(cb.FuncList.WallReplyDelete, f)
+	cb.funcList.WallReplyDelete = append(cb.funcList.WallReplyDelete, f)
 }
 
 // BoardPostNew handler
 func (cb *Callback) BoardPostNew(f object.BoardPostNewFunc) {
-	cb.FuncList.BoardPostNew = append(cb.FuncList.BoardPostNew, f)
+	cb.funcList.BoardPostNew = append(cb.funcList.BoardPostNew, f)
 }
 
 // BoardPostEdit handler
 func (cb *Callback) BoardPostEdit(f object.BoardPostEditFunc) {
-	cb.FuncList.BoardPostEdit = append(cb.FuncList.BoardPostEdit, f)
+	cb.funcList.BoardPostEdit = append(cb.funcList.BoardPostEdit, f)
 }
 
 // BoardPostRestore handler
 func (cb *Callback) BoardPostRestore(f object.BoardPostRestoreFunc) {
-	cb.FuncList.BoardPostRestore = append(cb.FuncList.BoardPostRestore, f)
+	cb.funcList.BoardPostRestore = append(cb.funcList.BoardPostRestore, f)
 }
 
 // BoardPostDelete handler
 func (cb *Callback) BoardPostDelete(f object.BoardPostDeleteFunc) {
-	cb.FuncList.BoardPostDelete = append(cb.FuncList.BoardPostDelete, f)
+	cb.funcList.BoardPostDelete = append(cb.funcList.BoardPostDelete, f)
 }
 
 // MarketCommentNew handler
 func (cb *Callback) MarketCommentNew(f object.MarketCommentNewFunc) {
-	cb.FuncList.MarketCommentNew = append(cb.FuncList.MarketCommentNew, f)
+	cb.funcList.MarketCommentNew = append(cb.funcList.MarketCommentNew, f)
 }
 
 // MarketCommentEdit handler
 func (cb *Callback) MarketCommentEdit(f object.MarketCommentEditFunc) {
-	cb.FuncList.MarketCommentEdit = append(cb.FuncList.MarketCommentEdit, f)
+	cb.funcList.MarketCommentEdit = append(cb.funcList.MarketCommentEdit, f)
 }
 
 // MarketCommentRestore handler
 func (cb *Callback) MarketCommentRestore(f object.MarketCommentRestoreFunc) {
-	cb.FuncList.MarketCommentRestore = append(cb.FuncList.MarketCommentRestore, f)
+	cb.funcList.MarketCommentRestore = append(cb.funcList.MarketCommentRestore, f)
 }
 
 // MarketCommentDelete handler
 func (cb *Callback) MarketCommentDelete(f object.MarketCommentDeleteFunc) {
-	cb.FuncList.MarketCommentDelete = append(cb.FuncList.MarketCommentDelete, f)
+	cb.funcList.MarketCommentDelete = append(cb.funcList.MarketCommentDelete, f)
 }
 
 // GroupLeave handler
 func (cb *Callback) GroupLeave(f object.GroupLeaveFunc) {
-	cb.FuncList.GroupLeave = append(cb.FuncList.GroupLeave, f)
+	cb.funcList.GroupLeave = append(cb.funcList.GroupLeave, f)
 }
 
 // GroupJoin handler
 func (cb *Callback) GroupJoin(f object.GroupJoinFunc) {
-	cb.FuncList.GroupJoin = append(cb.FuncList.GroupJoin, f)
+	cb.funcList.GroupJoin = append(cb.funcList.GroupJoin, f)
 }
 
 // UserBlock handler
 func (cb *Callback) UserBlock(f object.UserBlockFunc) {
-	cb.FuncList.UserBlock = append(cb.FuncList.UserBlock, f)
+	cb.funcList.UserBlock = append(cb.funcList.UserBlock, f)
 }
 
 // UserUnblock handler
 func (cb *Callback) UserUnblock(f object.UserUnblockFunc) {
-	cb.FuncList.UserUnblock = append(cb.FuncList.UserUnblock, f)
+	cb.funcList.UserUnblock = append(cb.funcList.UserUnblock, f)
 }
 
 // PollVoteNew handler
 func (cb *Callback) PollVoteNew(f object.PollVoteNewFunc) {
-	cb.FuncList.PollVoteNew = append(cb.FuncList.PollVoteNew, f)
+	cb.funcList.PollVoteNew = append(cb.funcList.PollVoteNew, f)
 }
 
 // GroupOfficersEdit handler
 func (cb *Callback) GroupOfficersEdit(f object.GroupOfficersEditFunc) {
-	cb.FuncList.GroupOfficersEdit = append(cb.FuncList.GroupOfficersEdit, f)
+	cb.funcList.GroupOfficersEdit = append(cb.funcList.GroupOfficersEdit, f)
 }
 
 // GroupChangeSettings handler
 func (cb *Callback) GroupChangeSettings(f object.GroupChangeSettingsFunc) {
-	cb.FuncList.GroupChangeSettings = append(cb.FuncList.GroupChangeSettings, f)
+	cb.funcList.GroupChangeSettings = append(cb.funcList.GroupChangeSettings, f)
 }
 
 // GroupChangePhoto handler
 func (cb *Callback) GroupChangePhoto(f object.GroupChangePhotoFunc) {
-	cb.FuncList.GroupChangePhoto = append(cb.FuncList.GroupChangePhoto, f)
+	cb.funcList.GroupChangePhoto = append(cb.funcList.GroupChangePhoto, f)
 }
 
 // VkpayTransaction handler
 func (cb *Callback) VkpayTransaction(f object.VkpayTransactionFunc) {
-	cb.FuncList.VkpayTransaction = append(cb.FuncList.VkpayTransaction, f)
+	cb.funcList.VkpayTransaction = append(cb.funcList.VkpayTransaction, f)
 }
 
 // LeadFormsNew handler
 func (cb *Callback) LeadFormsNew(f object.LeadFormsNewFunc) {
-	cb.FuncList.LeadFormsNew = append(cb.FuncList.LeadFormsNew, f)
+	cb.funcList.LeadFormsNew = append(cb.funcList.LeadFormsNew, f)
 }
 
 // TODO: next version like_add handler
