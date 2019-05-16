c78363dc045ca790b63711db5f9abe4d5b2b6d37
Suvorov Daniil <severecloud@gmail.com>
2019-05-16 16:36:11 +0300

    fix: test Handler

--- 5.92/callback/callback_test.go
+++ 5.92/callback/callback_test.go
@@ -6,330 +6,252 @@ import (
 	"github.com/SevereCloud/vksdk/5.92/object"
 )
 
-func TestCallback_MessageNew(t *testing.T) {
+func TestCallback_Handler(t *testing.T) {
+	cb := &Callback{}
 	t.Run("MessageNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MessageNew(func(obj object.MessageNewObject, groupID int) {})
+		if len(cb.funcList.MessageNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MessageReply(t *testing.T) {
 	t.Run("MessageReply", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MessageReply(func(obj object.MessageReplyObject, groupID int) {})
+		if len(cb.funcList.MessageReply) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MessageEdit(t *testing.T) {
 	t.Run("MessageEdit", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MessageEdit(func(obj object.MessageEditObject, groupID int) {})
+		if len(cb.funcList.MessageEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MessageAllow(t *testing.T) {
 	t.Run("MessageAllow", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MessageAllow(func(obj object.MessageAllowObject, groupID int) {})
+		if len(cb.funcList.MessageAllow) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MessageDeny(t *testing.T) {
 	t.Run("MessageDeny", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MessageDeny(func(obj object.MessageDenyObject, groupID int) {})
+		if len(cb.funcList.MessageDeny) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MessageTypingState(t *testing.T) {
 	t.Run("MessageTypingState", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MessageTypingState(func(obj object.MessageTypingStateObject, groupID int) {})
+		if len(cb.funcList.MessageTypingState) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_PhotoNew(t *testing.T) {
 	t.Run("PhotoNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.PhotoNew(func(obj object.PhotoNewObject, groupID int) {})
+		if len(cb.funcList.PhotoNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_PhotoCommentNew(t *testing.T) {
 	t.Run("PhotoCommentNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.PhotoCommentNew(func(obj object.PhotoCommentNewObject, groupID int) {})
+		if len(cb.funcList.PhotoCommentNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_PhotoCommentEdit(t *testing.T) {
 	t.Run("PhotoCommentEdit", func(t *testing.T) {
-		cb := &Callback{}
 		cb.PhotoCommentEdit(func(obj object.PhotoCommentEditObject, groupID int) {})
+		if len(cb.funcList.PhotoCommentEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_PhotoCommentRestore(t *testing.T) {
 	t.Run("PhotoCommentRestore", func(t *testing.T) {
-		cb := &Callback{}
 		cb.PhotoCommentRestore(func(obj object.PhotoCommentRestoreObject, groupID int) {})
+		if len(cb.funcList.PhotoCommentRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_PhotoCommentDelete(t *testing.T) {
 	t.Run("PhotoCommentDelete", func(t *testing.T) {
-		cb := &Callback{}
 		cb.PhotoCommentDelete(func(obj object.PhotoCommentDeleteObject, groupID int) {})
+		if len(cb.funcList.PhotoCommentDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_AudioNew(t *testing.T) {
 	t.Run("AudioNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.AudioNew(func(obj object.AudioNewObject, groupID int) {})
+		if len(cb.funcList.AudioNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_VideoNew(t *testing.T) {
 	t.Run("VideoNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.VideoNew(func(obj object.VideoNewObject, groupID int) {})
+		if len(cb.funcList.VideoNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_VideoCommentNew(t *testing.T) {
 	t.Run("VideoCommentNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.VideoCommentNew(func(obj object.VideoCommentNewObject, groupID int) {})
+		if len(cb.funcList.VideoCommentNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_VideoCommentEdit(t *testing.T) {
 	t.Run("VideoCommentEdit", func(t *testing.T) {
-		cb := &Callback{}
 		cb.VideoCommentEdit(func(obj object.VideoCommentEditObject, groupID int) {})
+		if len(cb.funcList.VideoCommentEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_VideoCommentRestore(t *testing.T) {
 	t.Run("VideoCommentRestore", func(t *testing.T) {
-		cb := &Callback{}
 		cb.VideoCommentRestore(func(obj object.VideoCommentRestoreObject, groupID int) {})
+		if len(cb.funcList.VideoCommentRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_VideoCommentDelete(t *testing.T) {
 	t.Run("VideoCommentDelete", func(t *testing.T) {
-		cb := &Callback{}
 		cb.VideoCommentDelete(func(obj object.VideoCommentDeleteObject, groupID int) {})
+		if len(cb.funcList.VideoCommentDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_WallPostNew(t *testing.T) {
 	t.Run("WallPostNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.WallPostNew(func(obj object.WallPostNewObject, groupID int) {})
+		if len(cb.funcList.WallPostNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_WallRepost(t *testing.T) {
 	t.Run("WallRepost", func(t *testing.T) {
-		cb := &Callback{}
 		cb.WallRepost(func(obj object.WallRepostObject, groupID int) {})
+		if len(cb.funcList.WallRepost) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_WallReplyNew(t *testing.T) {
 	t.Run("WallReplyNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.WallReplyNew(func(obj object.WallReplyNewObject, groupID int) {})
+		if len(cb.funcList.WallReplyNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_WallReplyEdit(t *testing.T) {
 	t.Run("WallReplyEdit", func(t *testing.T) {
-		cb := &Callback{}
 		cb.WallReplyEdit(func(obj object.WallReplyEditObject, groupID int) {})
+		if len(cb.funcList.WallReplyEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_WallReplyRestore(t *testing.T) {
 	t.Run("WallReplyRestore", func(t *testing.T) {
-		cb := &Callback{}
 		cb.WallReplyRestore(func(obj object.WallReplyRestoreObject, groupID int) {})
+		if len(cb.funcList.WallReplyRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_WallReplyDelete(t *testing.T) {
 	t.Run("WallReplyDelete", func(t *testing.T) {
-		cb := &Callback{}
 		cb.WallReplyDelete(func(obj object.WallReplyDeleteObject, groupID int) {})
+		if len(cb.funcList.WallReplyDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_BoardPostNew(t *testing.T) {
 	t.Run("BoardPostNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.BoardPostNew(func(obj object.BoardPostNewObject, groupID int) {})
+		if len(cb.funcList.BoardPostNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_BoardPostEdit(t *testing.T) {
 	t.Run("BoardPostEdit", func(t *testing.T) {
-		cb := &Callback{}
 		cb.BoardPostEdit(func(obj object.BoardPostEditObject, groupID int) {})
+		if len(cb.funcList.BoardPostEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_BoardPostRestore(t *testing.T) {
 	t.Run("BoardPostRestore", func(t *testing.T) {
-		cb := &Callback{}
 		cb.BoardPostRestore(func(obj object.BoardPostRestoreObject, groupID int) {})
+		if len(cb.funcList.BoardPostRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_BoardPostDelete(t *testing.T) {
 	t.Run("BoardPostDelete", func(t *testing.T) {
-		cb := &Callback{}
 		cb.BoardPostDelete(func(obj object.BoardPostDeleteObject, groupID int) {})
+		if len(cb.funcList.BoardPostDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MarketCommentNew(t *testing.T) {
 	t.Run("MarketCommentNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MarketCommentNew(func(obj object.MarketCommentNewObject, groupID int) {})
+		if len(cb.funcList.MarketCommentNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MarketCommentEdit(t *testing.T) {
 	t.Run("MarketCommentEdit", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MarketCommentEdit(func(obj object.MarketCommentEditObject, groupID int) {})
+		if len(cb.funcList.MarketCommentEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MarketCommentRestore(t *testing.T) {
 	t.Run("MarketCommentRestore", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MarketCommentRestore(func(obj object.MarketCommentRestoreObject, groupID int) {})
+		if len(cb.funcList.MarketCommentRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_MarketCommentDelete(t *testing.T) {
 	t.Run("MarketCommentDelete", func(t *testing.T) {
-		cb := &Callback{}
 		cb.MarketCommentDelete(func(obj object.MarketCommentDeleteObject, groupID int) {})
+		if len(cb.funcList.MarketCommentDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_GroupLeave(t *testing.T) {
 	t.Run("GroupLeave", func(t *testing.T) {
-		cb := &Callback{}
 		cb.GroupLeave(func(obj object.GroupLeaveObject, groupID int) {})
+		if len(cb.funcList.GroupLeave) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_GroupJoin(t *testing.T) {
 	t.Run("GroupJoin", func(t *testing.T) {
-		cb := &Callback{}
 		cb.GroupJoin(func(obj object.GroupJoinObject, groupID int) {})
+		if len(cb.funcList.GroupJoin) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_UserBlock(t *testing.T) {
 	t.Run("UserBlock", func(t *testing.T) {
-		cb := &Callback{}
 		cb.UserBlock(func(obj object.UserBlockObject, groupID int) {})
+		if len(cb.funcList.UserBlock) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_UserUnblock(t *testing.T) {
 	t.Run("UserUnblock", func(t *testing.T) {
-		cb := &Callback{}
 		cb.UserUnblock(func(obj object.UserUnblockObject, groupID int) {})
+		if len(cb.funcList.UserUnblock) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_PollVoteNew(t *testing.T) {
 	t.Run("PollVoteNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.PollVoteNew(func(obj object.PollVoteNewObject, groupID int) {})
+		if len(cb.funcList.PollVoteNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_GroupOfficersEdit(t *testing.T) {
 	t.Run("GroupOfficersEdit", func(t *testing.T) {
-		cb := &Callback{}
 		cb.GroupOfficersEdit(func(obj object.GroupOfficersEditObject, groupID int) {})
+		if len(cb.funcList.GroupOfficersEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_GroupChangeSettings(t *testing.T) {
 	t.Run("GroupChangeSettings", func(t *testing.T) {
-		cb := &Callback{}
 		cb.GroupChangeSettings(func(obj object.GroupChangeSettingsObject, groupID int) {})
+		if len(cb.funcList.GroupChangeSettings) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_GroupChangePhoto(t *testing.T) {
 	t.Run("GroupChangePhoto", func(t *testing.T) {
-		cb := &Callback{}
 		cb.GroupChangePhoto(func(obj object.GroupChangePhotoObject, groupID int) {})
+		if len(cb.funcList.GroupChangePhoto) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_VkpayTransaction(t *testing.T) {
 	t.Run("VkpayTransaction", func(t *testing.T) {
-		cb := &Callback{}
 		cb.VkpayTransaction(func(obj object.VkpayTransactionObject, groupID int) {})
+		if len(cb.funcList.VkpayTransaction) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestCallback_LeadFormsNew(t *testing.T) {
 	t.Run("LeadFormsNew", func(t *testing.T) {
-		cb := &Callback{}
 		cb.LeadFormsNew(func(obj object.LeadFormsNewObject, groupID int) {})
+		if len(cb.funcList.LeadFormsNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
 }
--- 5.92/longpoll-bot/longpoll_test.go
+++ 5.92/longpoll-bot/longpoll_test.go
@@ -20,332 +20,254 @@ func TestLongpoll_Shutdown(t *testing.T) {
 
 }
 
-func TestLongpoll_MessageNew(t *testing.T) {
+func TestLongpoll_Handler(t *testing.T) {
+	lp := &Longpoll{}
 	t.Run("MessageNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MessageNew(func(obj object.MessageNewObject, groupID int) {})
+		if len(lp.funcList.MessageNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MessageReply(t *testing.T) {
 	t.Run("MessageReply", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MessageReply(func(obj object.MessageReplyObject, groupID int) {})
+		if len(lp.funcList.MessageReply) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MessageEdit(t *testing.T) {
 	t.Run("MessageEdit", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MessageEdit(func(obj object.MessageEditObject, groupID int) {})
+		if len(lp.funcList.MessageEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MessageAllow(t *testing.T) {
 	t.Run("MessageAllow", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MessageAllow(func(obj object.MessageAllowObject, groupID int) {})
+		if len(lp.funcList.MessageAllow) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MessageDeny(t *testing.T) {
 	t.Run("MessageDeny", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MessageDeny(func(obj object.MessageDenyObject, groupID int) {})
+		if len(lp.funcList.MessageDeny) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MessageTypingState(t *testing.T) {
 	t.Run("MessageTypingState", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MessageTypingState(func(obj object.MessageTypingStateObject, groupID int) {})
+		if len(lp.funcList.MessageTypingState) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_PhotoNew(t *testing.T) {
 	t.Run("PhotoNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.PhotoNew(func(obj object.PhotoNewObject, groupID int) {})
+		if len(lp.funcList.PhotoNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_PhotoCommentNew(t *testing.T) {
 	t.Run("PhotoCommentNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.PhotoCommentNew(func(obj object.PhotoCommentNewObject, groupID int) {})
+		if len(lp.funcList.PhotoCommentNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_PhotoCommentEdit(t *testing.T) {
 	t.Run("PhotoCommentEdit", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.PhotoCommentEdit(func(obj object.PhotoCommentEditObject, groupID int) {})
+		if len(lp.funcList.PhotoCommentEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_PhotoCommentRestore(t *testing.T) {
 	t.Run("PhotoCommentRestore", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.PhotoCommentRestore(func(obj object.PhotoCommentRestoreObject, groupID int) {})
+		if len(lp.funcList.PhotoCommentRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_PhotoCommentDelete(t *testing.T) {
 	t.Run("PhotoCommentDelete", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.PhotoCommentDelete(func(obj object.PhotoCommentDeleteObject, groupID int) {})
+		if len(lp.funcList.PhotoCommentDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_AudioNew(t *testing.T) {
 	t.Run("AudioNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.AudioNew(func(obj object.AudioNewObject, groupID int) {})
+		if len(lp.funcList.AudioNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_VideoNew(t *testing.T) {
 	t.Run("VideoNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.VideoNew(func(obj object.VideoNewObject, groupID int) {})
+		if len(lp.funcList.VideoNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_VideoCommentNew(t *testing.T) {
 	t.Run("VideoCommentNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.VideoCommentNew(func(obj object.VideoCommentNewObject, groupID int) {})
+		if len(lp.funcList.VideoCommentNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_VideoCommentEdit(t *testing.T) {
 	t.Run("VideoCommentEdit", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.VideoCommentEdit(func(obj object.VideoCommentEditObject, groupID int) {})
+		if len(lp.funcList.VideoCommentEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_VideoCommentRestore(t *testing.T) {
 	t.Run("VideoCommentRestore", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.VideoCommentRestore(func(obj object.VideoCommentRestoreObject, groupID int) {})
+		if len(lp.funcList.VideoCommentRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_VideoCommentDelete(t *testing.T) {
 	t.Run("VideoCommentDelete", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.VideoCommentDelete(func(obj object.VideoCommentDeleteObject, groupID int) {})
+		if len(lp.funcList.VideoCommentDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_WallPostNew(t *testing.T) {
 	t.Run("WallPostNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.WallPostNew(func(obj object.WallPostNewObject, groupID int) {})
+		if len(lp.funcList.WallPostNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_WallRepost(t *testing.T) {
 	t.Run("WallRepost", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.WallRepost(func(obj object.WallRepostObject, groupID int) {})
+		if len(lp.funcList.WallRepost) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_WallReplyNew(t *testing.T) {
 	t.Run("WallReplyNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.WallReplyNew(func(obj object.WallReplyNewObject, groupID int) {})
+		if len(lp.funcList.WallReplyNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_WallReplyEdit(t *testing.T) {
 	t.Run("WallReplyEdit", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.WallReplyEdit(func(obj object.WallReplyEditObject, groupID int) {})
+		if len(lp.funcList.WallReplyEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_WallReplyRestore(t *testing.T) {
 	t.Run("WallReplyRestore", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.WallReplyRestore(func(obj object.WallReplyRestoreObject, groupID int) {})
+		if len(lp.funcList.WallReplyRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_WallReplyDelete(t *testing.T) {
 	t.Run("WallReplyDelete", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.WallReplyDelete(func(obj object.WallReplyDeleteObject, groupID int) {})
+		if len(lp.funcList.WallReplyDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_BoardPostNew(t *testing.T) {
 	t.Run("BoardPostNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.BoardPostNew(func(obj object.BoardPostNewObject, groupID int) {})
+		if len(lp.funcList.BoardPostNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_BoardPostEdit(t *testing.T) {
 	t.Run("BoardPostEdit", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.BoardPostEdit(func(obj object.BoardPostEditObject, groupID int) {})
+		if len(lp.funcList.BoardPostEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_BoardPostRestore(t *testing.T) {
 	t.Run("BoardPostRestore", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.BoardPostRestore(func(obj object.BoardPostRestoreObject, groupID int) {})
+		if len(lp.funcList.BoardPostRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_BoardPostDelete(t *testing.T) {
 	t.Run("BoardPostDelete", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.BoardPostDelete(func(obj object.BoardPostDeleteObject, groupID int) {})
+		if len(lp.funcList.BoardPostDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MarketCommentNew(t *testing.T) {
 	t.Run("MarketCommentNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MarketCommentNew(func(obj object.MarketCommentNewObject, groupID int) {})
+		if len(lp.funcList.MarketCommentNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MarketCommentEdit(t *testing.T) {
 	t.Run("MarketCommentEdit", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MarketCommentEdit(func(obj object.MarketCommentEditObject, groupID int) {})
+		if len(lp.funcList.MarketCommentEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MarketCommentRestore(t *testing.T) {
 	t.Run("MarketCommentRestore", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MarketCommentRestore(func(obj object.MarketCommentRestoreObject, groupID int) {})
+		if len(lp.funcList.MarketCommentRestore) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_MarketCommentDelete(t *testing.T) {
 	t.Run("MarketCommentDelete", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.MarketCommentDelete(func(obj object.MarketCommentDeleteObject, groupID int) {})
+		if len(lp.funcList.MarketCommentDelete) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_GroupLeave(t *testing.T) {
 	t.Run("GroupLeave", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.GroupLeave(func(obj object.GroupLeaveObject, groupID int) {})
+		if len(lp.funcList.GroupLeave) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_GroupJoin(t *testing.T) {
 	t.Run("GroupJoin", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.GroupJoin(func(obj object.GroupJoinObject, groupID int) {})
+		if len(lp.funcList.GroupJoin) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_UserBlock(t *testing.T) {
 	t.Run("UserBlock", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.UserBlock(func(obj object.UserBlockObject, groupID int) {})
+		if len(lp.funcList.UserBlock) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_UserUnblock(t *testing.T) {
 	t.Run("UserUnblock", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.UserUnblock(func(obj object.UserUnblockObject, groupID int) {})
+		if len(lp.funcList.UserUnblock) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_PollVoteNew(t *testing.T) {
 	t.Run("PollVoteNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.PollVoteNew(func(obj object.PollVoteNewObject, groupID int) {})
+		if len(lp.funcList.PollVoteNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_GroupOfficersEdit(t *testing.T) {
 	t.Run("GroupOfficersEdit", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.GroupOfficersEdit(func(obj object.GroupOfficersEditObject, groupID int) {})
+		if len(lp.funcList.GroupOfficersEdit) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_GroupChangeSettings(t *testing.T) {
 	t.Run("GroupChangeSettings", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.GroupChangeSettings(func(obj object.GroupChangeSettingsObject, groupID int) {})
+		if len(lp.funcList.GroupChangeSettings) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_GroupChangePhoto(t *testing.T) {
 	t.Run("GroupChangePhoto", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.GroupChangePhoto(func(obj object.GroupChangePhotoObject, groupID int) {})
+		if len(lp.funcList.GroupChangePhoto) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_VkpayTransaction(t *testing.T) {
 	t.Run("VkpayTransaction", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.VkpayTransaction(func(obj object.VkpayTransactionObject, groupID int) {})
+		if len(lp.funcList.VkpayTransaction) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
-}
-
-func TestLongpoll_LeadFormsNew(t *testing.T) {
 	t.Run("LeadFormsNew", func(t *testing.T) {
-		lp := &Longpoll{}
 		lp.LeadFormsNew(func(obj object.LeadFormsNewObject, groupID int) {})
+		if len(lp.funcList.LeadFormsNew) != 1 {
+			t.Error("Want len = 1")
+		}
 	})
-
 }
 
 func TestInit(t *testing.T) {
@@ -441,7 +363,7 @@ func TestLongpoll_checkResponse(t *testing.T) {
 	}
 }
 
-// TODO: write
+// TODO: write TestLongpoll_Run
 // func TestLongpoll_Run(t *testing.T) {
 // 	groupToken := os.Getenv("GROUP_TOKEN")
 // 	if groupToken == "" {
