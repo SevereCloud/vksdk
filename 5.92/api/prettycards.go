033ae0b145cf9415614f3c565de157d1254d7d2d
Suvorov Daniil <severecloud@gmail.com>
2019-05-24 21:52:12 +0300

    feat(api): Add PrettyCards method #14

--- 5.92/api/prettycards.go
+++ 5.92/api/prettycards.go
@@ -1,43 +1,82 @@
 package api // import "github.com/SevereCloud/vksdk/5.92/api"
 
-// PrettyCardsCreateResponse struct
-type PrettyCardsCreateResponse struct{}
+import "github.com/SevereCloud/vksdk/5.92/object"
 
-// TODO: prettyCards.create
+// PrettyCardsCreateResponse struct
+type PrettyCardsCreateResponse struct {
+	OwnerID int    `json:"owner_id"` // Owner ID of created pretty card
+	CardID  string `json:"card_id"`  // Card ID of created pretty card
+}
+
+// PrettyCardsCreate
 //
 // https://vk.com/dev/prettyCards.create
+func (vk *VK) PrettyCardsCreate(params map[string]string) (response PrettyCardsCreateResponse, vkErr Error) {
+	vk.RequestUnmarshal("prettyCards.create", params, &response, &vkErr)
+	return
+}
 
 // PrettyCardsDeleteResponse struct
-type PrettyCardsDeleteResponse struct{}
+type PrettyCardsDeleteResponse struct {
+	OwnerID int    `json:"owner_id"` // Owner ID of created pretty card
+	CardID  string `json:"card_id"`  // Card ID of created pretty card
+	Error   string `json:"error"`    // Error reason if error happened
+}
 
-// TODO: prettyCards.delete
+// PrettyCardsDelete
 //
 // https://vk.com/dev/prettyCards.delete
+func (vk *VK) PrettyCardsDelete(params map[string]string) (response PrettyCardsDeleteResponse, vkErr Error) {
+	vk.RequestUnmarshal("prettyCards.delete", params, &response, &vkErr)
+	return
+}
 
 // PrettyCardsEditResponse struct
-type PrettyCardsEditResponse struct{}
+type PrettyCardsEditResponse struct {
+	OwnerID int    `json:"owner_id"` // Owner ID of created pretty card
+	CardID  string `json:"card_id"`  // Card ID of created pretty card
+}
 
-// TODO: prettyCards.edit
+// PrettyCardsEdit
 //
 // https://vk.com/dev/prettyCards.edit
+func (vk *VK) PrettyCardsEdit(params map[string]string) (response PrettyCardsEditResponse, vkErr Error) {
+	vk.RequestUnmarshal("prettyCards.edit", params, &response, &vkErr)
+	return
+}
 
 // PrettyCardsGetResponse struct
-type PrettyCardsGetResponse struct{}
+type PrettyCardsGetResponse struct {
+	Count int                            `json:"count"` // Total number
+	Items []object.PrettyCardsPrettyCard `json:"items"`
+}
 
-// TODO: prettyCards.get
+// PrettyCardsGet
 //
 // https://vk.com/dev/prettyCards.get
+func (vk *VK) PrettyCardsGet(params map[string]string) (response PrettyCardsGetResponse, vkErr Error) {
+	vk.RequestUnmarshal("prettyCards.get", params, &response, &vkErr)
+	return
+}
 
 // PrettyCardsGetByIDResponse struct
-type PrettyCardsGetByIDResponse struct{}
+type PrettyCardsGetByIDResponse []object.PrettyCardsPrettyCard
 
-// TODO: prettyCards.getById
+// PrettyCardsGetByID
 //
 // https://vk.com/dev/prettyCards.getById
+func (vk *VK) PrettyCardsGetByID(params map[string]string) (response PrettyCardsGetByIDResponse, vkErr Error) {
+	vk.RequestUnmarshal("prettyCards.getById", params, &response, &vkErr)
+	return
+}
 
 // PrettyCardsGetUploadURLResponse struct
-type PrettyCardsGetUploadURLResponse struct{}
+type PrettyCardsGetUploadURLResponse string // Upload URL
 
-// TODO: prettyCards.getUploadURL
+// PrettyCardsGetUploadURL
 //
 // https://vk.com/dev/prettyCards.getUploadURL
+func (vk *VK) PrettyCardsGetUploadURL() (response PrettyCardsGetUploadURLResponse, vkErr Error) {
+	vk.RequestUnmarshal("prettyCards.getUploadURL", map[string]string{}, &response, &vkErr)
+	return
+}
