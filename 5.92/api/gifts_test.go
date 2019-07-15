9016335123f8072c9f0cf95063d7a3a6af5e328a
Suvorov Daniil <severecloud@gmail.com>
2019-07-15 15:49:06 +0300

    test: gifts method

---
+++ 5.92/api/gifts_test.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestVK_GiftsGet(t *testing.T) {
+	if vkUser.AccessToken == "" {
+		t.Skip("USER_TOKEN empty")
+	}
+
+	t.Run("GiftsGet", func(t *testing.T) {
+		_, gotVkErr := vkUser.GiftsGet(map[string]string{
+			"user_id": "1",
+			"count":   "20",
+		})
+		if gotVkErr.Code != 0 {
+			t.Errorf("VK.GiftsGet() gotVkErr = %v, want %v", gotVkErr, 0)
+		}
+	})
+}
