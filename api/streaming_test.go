14ed0adaa8e674213c6711a30c957363f310b023
Suvorov Daniil <severecloud@gmail.com>
2020-02-26 12:29:15 +0300

    test: vk fix  StreamingGetStem

--- api/streaming_test.go
+++ api/streaming_test.go
@@ -3,8 +3,6 @@ package api_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/SevereCloud/vksdk/api"
 
 	"github.com/SevereCloud/vksdk/api/errors"
@@ -77,11 +75,9 @@ func TestVK_StreamingGetStem(t *testing.T) {
 			t.Errorf("%v", err)
 		}
 
-		assert.NotEmpty(t, response.Stem)
-		// BUG(VK): bad encoding
-		// if response.Stem != "собак" {
-		// 	t.Error("Sterm wrong")
-		// }
+		if response.Stem != "собак" {
+			t.Error("Sterm wrong")
+		}
 	})
 }
 
