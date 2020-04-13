cb21df56b04346afffa91d508d0c17b7ef336dde
Suvorov Daniil <severecloud@gmail.com>
2020-04-13 13:15:19 +0300

    test: fix lint

--- api/api_test.go
+++ api/api_test.go
@@ -27,6 +27,7 @@ func noError(t *testing.T, err error) bool {
 	case errors.Captcha:
 		t.Skip("Captcha needed")
 	}
+
 	if err != nil {
 		ctx := errors.GetErrorContext(err)
 		if ctx.Code != 0 {
@@ -35,9 +36,11 @@ func noError(t *testing.T, err error) bool {
 			s += fmt.Sprintf("text: %s\n", ctx.Text)
 			s += fmt.Sprintf("message: %s\n", ctx.Message)
 			s += "params:\n"
+
 			for _, param := range ctx.RequestParams {
 				s += fmt.Sprintf("\t%s: %s\n", param.Key, param.Value)
 			}
+
 			t.Log(s)
 		}
 	}
