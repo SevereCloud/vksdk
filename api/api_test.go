b9ae23c253ad287caafdeaad5493af4ea8691896
geosonic <36260850+geosonic@users.noreply.github.com>
2020-05-19 13:28:03 +0500

    feat: add ExecuteWithArgs method

--- api/api_test.go
+++ api/api_test.go
@@ -211,6 +211,32 @@ func TestVK_Execute_object(t *testing.T) {
 	assert.Equal(t, "hello", response.Text)
 }
 
+func TestVK_ExecuteWithArgs_error(t *testing.T) {
+	t.Parallel()
+
+	needGroupToken(t)
+
+	var response int
+
+	err := vkGroup.ExecuteWithArgs(`API.users.get({user_id: parseInt(Args.user_id)});return 1;`, api.Params{"user_id": -1}, &response)
+	assert.Error(t, err)
+	assert.Equal(t, 1, response)
+}
+
+func TestVK_ExecuteWithArgs_object(t *testing.T) {
+	t.Parallel()
+
+	needGroupToken(t)
+
+	var response struct {
+		Text string `json:"text"`
+	}
+
+	err := vkGroup.ExecuteWithArgs(`return {text: Args.text};`, api.Params{"text": "hello"}, &response)
+	assert.NoError(t, err)
+	assert.Equal(t, "hello", response.Text)
+}
+
 func TestVK_InvalidContentType(t *testing.T) {
 	t.Parallel()
 
