03ac011456d0b19202b8a85ce8d66656424b0d2c
Suvorov Daniil <severecloud@gmail.com>
2020-06-06 23:58:55 +0300

    feat: add AdsRemoveTargetContacts method

--- api/ads.go
+++ api/ads.go
@@ -30,3 +30,17 @@ func (vk *VK) AdsGetAccounts(params Params) (response AdsGetAccountsResponse, er
 	err = vk.RequestUnmarshal("ads.getAccounts", params, &response)
 	return
 }
+
+// AdsRemoveTargetContacts accepts the request to exclude the advertiser's
+// contacts from the retargeting audience.
+//
+// The maximum allowed number of contacts to be excluded by a single
+// request is 1000.
+//
+// Contacts are excluded within a few hours of the request.
+//
+// https://vk.com/dev/ads.removeTargetContacts
+func (vk *VK) AdsRemoveTargetContacts(params Params) (response int, err error) {
+	err = vk.RequestUnmarshal("ads.removeTargetContacts", params, &response)
+	return
+}
