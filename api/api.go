fa3c897af3ea0355cd099f39d592087f757d54e7
Suvorov Daniil <severecloud@gmail.com>
2020-08-23 21:43:53 +0300

    refactor: separate file for execute

--- api/api.go
+++ api/api.go
@@ -303,61 +303,6 @@ func (vk *VK) RequestUnmarshal(method string, params Params, obj interface{}) er
 	return json.Unmarshal(rawResponse, &obj)
 }
 
-// ExecuteWithArgs a universal method for calling a sequence of other methods
-// while saving and filtering interim results.
-//
-// The Args map variable allows you to retrieve the parameters passed during
-// the request and avoids code formatting.
-//
-// 	return Args.code; // return parameter "code"
-// 	return Args.v; // return parameter "v"
-//
-// https://vk.com/dev/execute
-func (vk *VK) ExecuteWithArgs(code string, params Params, obj interface{}) error {
-	token := vk.AccessToken
-	if vk.IsPoolClient {
-		token = vk.tokenPool.Get()
-	}
-
-	copyParams := make(Params)
-	for key, value := range params {
-		copyParams[key] = FmtValue(value, 0)
-	}
-
-	copyParams["code"] = code
-	copyParams["access_token"] = token
-	copyParams["v"] = vk.Version
-
-	resp, err := vk.Handler("execute", copyParams)
-
-	jsonErr := json.Unmarshal(resp.Response, &obj)
-	if jsonErr != nil {
-		return jsonErr
-	}
-
-	if resp.ExecuteErrors != nil {
-		return &resp.ExecuteErrors
-	}
-
-	return err
-}
-
-// Execute a universal method for calling a sequence of other methods while
-// saving and filtering interim results.
-//
-// https://vk.com/dev/execute
-func (vk *VK) Execute(code string, obj interface{}) error {
-	return vk.ExecuteWithArgs(code, Params{}, obj)
-}
-
-func fmtBool(value bool) string {
-	if value {
-		return "1"
-	}
-
-	return "0"
-}
-
 func fmtReflectValue(value reflect.Value, depth int) string {
 	switch f := value; value.Kind() {
 	case reflect.Invalid:
--- api/api_test.go
+++ api/api_test.go
@@ -183,62 +183,6 @@ func TestVK_RequestLimit(t *testing.T) {
 	vkUser.Limit = 3
 }
 
-func TestVK_Execute_error(t *testing.T) {
-	t.Parallel()
-
-	needGroupToken(t)
-
-	var response int
-
-	err := vkGroup.Execute(`API.users.get({user_id:-1});return 1;`, &response)
-	assert.Error(t, err)
-	assert.Equal(t, 1, response)
-}
-
-func TestVK_Execute_object(t *testing.T) {
-	t.Parallel()
-
-	needGroupToken(t)
-
-	var response struct {
-		Text string `json:"text"`
-	}
-
-	err := vkGroup.Execute(`return {text: "hello"};`, &response)
-	assert.NoError(t, err)
-	assert.Equal(t, "hello", response.Text)
-}
-
-func TestVK_ExecuteWithArgs_error(t *testing.T) {
-	t.Parallel()
-
-	needGroupToken(t)
-
-	var response int
-
-	err := vkGroup.ExecuteWithArgs(
-		`API.users.get({user_id: parseInt(Args.user_id)});return 1;`,
-		api.Params{"user_id": -1},
-		&response,
-	)
-	assert.Error(t, err)
-	assert.Equal(t, 1, response)
-}
-
-func TestVK_ExecuteWithArgs_object(t *testing.T) {
-	t.Parallel()
-
-	needGroupToken(t)
-
-	var response struct {
-		Text string `json:"text"`
-	}
-
-	err := vkGroup.ExecuteWithArgs(`return {text: Args.text};`, api.Params{"text": "hello"}, &response)
-	assert.NoError(t, err)
-	assert.Equal(t, "hello", response.Text)
-}
-
 func TestVK_InvalidContentType(t *testing.T) {
 	t.Parallel()
 
