75c976d8d0a90e486e986bc14d04ed28a75c4e04
Suvorov Daniil <severecloud@gmail.com>
2020-09-09 20:31:47 +0300

    feat: marusia NewItemsList

--- marusia/skill.go
+++ marusia/skill.go
@@ -91,7 +91,16 @@ Webhook URL, который вы указали в настройках.
 				resp.Text = "Скилл запущен"
 				resp.TTS = "Скилл запущен, жду команд"
 			case "картинка":
-				resp.Card = marusia.NewBigImage("Заголовок", "Описание", 457239017)
+				resp.Card = marusia.NewBigImage(
+					"Заголовок",
+					"Описание",
+					457239017,
+				)
+			case "картинки":
+				resp.Card = marusia.NewImageList(
+					457239017,
+					457239018,
+				)
 			case "кнопки":
 				resp.Text = "Держи кнопки"
 				resp.TTS = "Жми на кнопки"
@@ -300,11 +309,17 @@ const (
 	ItemsList CardType = "ItemsList"
 )
 
-// TODO: CardItem элемент карточки.
-// type CardItem struct {
-// 	// ID изображения из раздела "Медиа-файлы" настроек в VKApps.
-// 	ImageID int `json:"image_id"`
-// }
+// CardItem элемент карточки.
+type CardItem struct {
+	// TODO: Заголовок изображения.
+	// Title string `json:"title,omitempty"`
+
+	// TODO: Описание изображения.
+	// Description string `json:"description,omitempty"`
+
+	// ID изображения из раздела "Медиа-файлы" настроек в VKApps.
+	ImageID int `json:"image_id"`
+}
 
 // Card описание карточки — сообщения с поддержкой изображений.
 type Card struct {
@@ -312,17 +327,17 @@ type Card struct {
 	Type CardType `json:"type"`
 
 	// Заголовок изображения.
-	Title string `json:"title"`
+	Title string `json:"title,omitempty"`
 
 	// Описание изображения.
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 
 	// ID изображения из раздела "Медиа-файлы" настроек в VKApps
 	// (игнорируется для типа ItemsList).
 	ImageID int `json:"image_id,omitempty"`
 
-	// TODO: Список изображений, каждый элемент является объектом формата BigImage.
-	// Items []CardItem `json:"items,omitempty"`
+	// Список изображений, каждый элемент является объектом формата BigImage.
+	Items []CardItem `json:"items,omitempty"`
 }
 
 // NewBigImage возвращает карточку с картинкой.
@@ -335,26 +350,24 @@ func NewBigImage(title, description string, imageID int) *Card {
 	}
 }
 
-// TODO: NewItemsList возвращает карточку с набором картинок.
-// func NewItemsList(title, description string, items []CardItem) *Card {
-// 	return &Card{
-// 		Type:        ItemsList,
-// 		Title:       title,
-// 		Description: description,
-// 		Items:       items,
-// 	}
-// }
+// NewItemsList возвращает карточку с набором картинок.
+func NewItemsList(items ...CardItem) *Card {
+	return &Card{
+		Type:  ItemsList,
+		Items: items,
+	}
+}
 
-// TODO: NewImageList возвращает карточку с набором картинок.
-// func NewImageList(title, description string, imageIDs ...int) *Card {
-// 	items := make([]CardItem, len(imageIDs))
+// NewImageList возвращает карточку с набором картинок.
+func NewImageList(imageIDs ...int) *Card {
+	items := make([]CardItem, len(imageIDs))
 
-// 	for i := 0; i < len(imageIDs); i++ {
-// 		items[i].ImageID = imageIDs[i]
-// 	}
+	for i := 0; i < len(imageIDs); i++ {
+		items[i].ImageID = imageIDs[i]
+	}
 
-// 	return NewItemsList(title, description, items)
-// }
+	return NewItemsList(items...)
+}
 
 // Response данные для ответа пользователю.
 type Response struct {
