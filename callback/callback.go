42684213b1be056188c2f72d077fcebba5dff350
Suvorov Daniil <severecloud@gmail.com>
2020-09-25 11:51:07 +0300

    feat(callback): add ErrorLog

--- callback/callback.go
+++ callback/callback.go
@@ -23,6 +23,11 @@ type Callback struct {
 	SecretKey        string
 	Title            string
 
+	// ErrorLog specifies an optional logger for errors unexpected
+	// behavior from handlers.
+	// If nil, logging is done via the log package's standard logger.
+	ErrorLog *log.Logger
+
 	events.FuncList
 }
 
@@ -44,7 +49,7 @@ func (cb *Callback) HandleFunc(w http.ResponseWriter, r *http.Request) {
 
 	var e events.GroupEvent
 	if err := decoder.Decode(&e); err != nil {
-		log.Printf("Callback.HandleFunc: %v", err)
+		cb.logf("callback: %v", err)
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 
 		return
@@ -52,7 +57,9 @@ func (cb *Callback) HandleFunc(w http.ResponseWriter, r *http.Request) {
 
 	if cb.SecretKeys[e.GroupID] != "" || cb.SecretKey != "" {
 		if e.Secret != cb.SecretKeys[e.GroupID] && e.Secret != cb.SecretKey {
+			cb.logf("callback: bad secret %d", e.GroupID)
 			http.Error(w, "Bad Secret", http.StatusForbidden)
+
 			return
 		}
 	}
@@ -68,11 +75,19 @@ func (cb *Callback) HandleFunc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := cb.Handler(context.TODO(), e); err != nil {
-		log.Printf("Callback.HandleFunc: %v", err)
+		cb.logf("callback: %v", err)
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 
 		return
 	}
 
-	fmt.Fprintf(w, "ok")
+	_, _ = w.Write([]byte("ok"))
+}
+
+func (cb *Callback) logf(format string, args ...interface{}) {
+	if cb.ErrorLog != nil {
+		cb.ErrorLog.Printf(format, args...)
+	} else {
+		log.Printf(format, args...)
+	}
 }
