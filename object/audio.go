986e264e68b40224655cfeee0771d51d0daad720
Suvorov Daniil <severecloud@gmail.com>
2020-09-27 17:44:34 +0300

    feat: AudioAudio add StoriesCoverAllowed

--- object/audio.go
+++ object/audio.go
@@ -6,28 +6,29 @@ import (
 
 // AudioAudio struct.
 type AudioAudio struct {
-	AccessKey          string             `json:"access_key"` // Access key for the audio
-	ID                 int                `json:"id"`
-	OwnerID            int                `json:"owner_id"`
-	Artist             string             `json:"artist"`
-	Title              string             `json:"title"`
-	Duration           int                `json:"duration"`
-	Date               int                `json:"date"`
-	URL                string             `json:"url"`
-	IsHq               BaseBoolInt        `json:"is_hq"`
-	IsExplicit         BaseBoolInt        `json:"is_explicit"`
-	StoriesAllowed     BaseBoolInt        `json:"stories_allowed"`
-	ShortVideosAllowed BaseBoolInt        `json:"short_videos_allowed"`
-	IsFocusTrack       BaseBoolInt        `json:"is_focus_track"`
-	IsLicensed         BaseBoolInt        `json:"is_licensed"`
-	LyricsID           int                `json:"lyrics_id"`
-	AlbumID            int                `json:"album_id"`
-	GenreID            int                `json:"genre_id"`
-	TrackCode          string             `json:"track_code"`
-	NoSearch           int                `json:"no_search"`
-	MainArtists        []AudioAudioArtist `json:"main_artists"`
-	Ads                AudioAds           `json:"ads"`
-	Subtitle           string             `json:"subtitle"`
+	AccessKey           string             `json:"access_key"` // Access key for the audio
+	ID                  int                `json:"id"`
+	OwnerID             int                `json:"owner_id"`
+	Artist              string             `json:"artist"`
+	Title               string             `json:"title"`
+	Duration            int                `json:"duration"`
+	Date                int                `json:"date"`
+	URL                 string             `json:"url"`
+	IsHq                BaseBoolInt        `json:"is_hq"`
+	IsExplicit          BaseBoolInt        `json:"is_explicit"`
+	StoriesAllowed      BaseBoolInt        `json:"stories_allowed"`
+	ShortVideosAllowed  BaseBoolInt        `json:"short_videos_allowed"`
+	IsFocusTrack        BaseBoolInt        `json:"is_focus_track"`
+	IsLicensed          BaseBoolInt        `json:"is_licensed"`
+	StoriesCoverAllowed BaseBoolInt        `json:"stories_cover_allowed"`
+	LyricsID            int                `json:"lyrics_id"`
+	AlbumID             int                `json:"album_id"`
+	GenreID             int                `json:"genre_id"`
+	TrackCode           string             `json:"track_code"`
+	NoSearch            int                `json:"no_search"`
+	MainArtists         []AudioAudioArtist `json:"main_artists"`
+	Ads                 AudioAds           `json:"ads"`
+	Subtitle            string             `json:"subtitle"`
 }
 
 // ToAttachment return attachment format.
