416581e5334f4d5a52614f6f69156fd28dc36020
rs <yuiop1955@mail.ru>
2020-10-07 18:39:53 +0500

    move tests to packer folder

--- api/packer/packer_test.go
+++ api/packer/packer_test.go
@@ -1,4 +1,4 @@
-package e2e
+package packer_test
 
 import (
 	"os"
@@ -49,5 +49,35 @@ func TestPacker(t *testing.T) {
 		assert.Nil(t, err)
 	}()
 
+	wg.Wait()
+}
+
+func TestManyAPICalls(t *testing.T) {
+	token := os.Getenv("USER_TOKEN")
+	if token == "" {
+		t.Skip("USER_TOKEN empty")
+	}
+
+	num := 500
+	vk := api.NewVK(token)
+	vk.Limit = api.LimitUserToken
+	packer.Default(vk)
+
+	var wg sync.WaitGroup
+
+	wg.Add(num)
+
+	for i := 0; i < num; i++ {
+		go func() {
+			defer wg.Done()
+
+			resp, err := vk.UtilsResolveScreenName(api.Params{
+				"screen_name": "durov",
+			})
+
+			assert.Nil(t, err)
+			assert.Equal(t, 1, resp.ObjectID)
+		}()
+	}
 	wg.Wait()
 }
