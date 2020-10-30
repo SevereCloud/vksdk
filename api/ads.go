be4477948b47776aa03c78599665ab75db2e142a
Suvorov Daniil <severecloud@gmail.com>
2020-10-30 11:09:38 +0300

    feat: add AdsCreateTargetGroup method

--- api/ads.go
+++ api/ads.go
@@ -114,16 +114,30 @@ func (vk *VK) AdsCreateClients(params Params) (response AdsCreateClientsResponse
 // 	return
 // }
 
-// TODO: AdsCreateTargetGroupResponse struct.
-// type AdsCreateTargetGroupResponse struct{}
+// AdsCreateTargetGroupResponse struct.
+type AdsCreateTargetGroupResponse struct {
+	ID int `json:"id"`
+}
 
-// TODO: AdsCreateTargetGroup ...
+// AdsCreateTargetGroup Creates a group to re-target ads for users who visited
+// advertiser's site (viewed information about the product, registered, etc.).
+//
+// When executed successfully this method returns user accounting code on
+// advertiser's site. You shall add this code to the site page, so users
+// registered in VK will be added to the created target group after they visit
+// this page.
+//
+// Use ads.importTargetContacts method to import existing user contacts to
+// the group.
+//
+// Please note! Maximum allowed number of groups for one advertising
+// account is 100.
 //
 // https://vk.com/dev/ads.createTargetGroup
-// func (vk *VK) AdsCreateTargetGroup(params Params) (response AdsCreateTargetGroupResponse, err error) {
-// 	err = vk.RequestUnmarshal("ads.createTargetGroup", &response, params)
-// 	return
-// }
+func (vk *VK) AdsCreateTargetGroup(params Params) (response AdsCreateTargetGroupResponse, err error) {
+	err = vk.RequestUnmarshal("ads.createTargetGroup", &response, params)
+	return
+}
 
 // TODO: AdsCreateTargetPixelResponse struct.
 // type AdsCreateTargetPixelResponse struct{}
