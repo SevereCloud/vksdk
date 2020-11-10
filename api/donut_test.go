2f77f997d710be6b2e4d6a5d7c51bdb5da6fac9d
Suvorov Daniil <severecloud@gmail.com>
2020-11-10 19:27:27 +0300

    test: donut methods

--- api/donut_test.go
+++ api/donut_test.go
@@ -1,21 +1,52 @@
 package api_test
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/SevereCloud/vksdk/v2/api"
 )
 
 func TestVK_DonutGetFriends(t *testing.T) {
-	// TODO: v5.125
+	t.Parallel()
+
+	needUserToken(t)
+
+	_, err := vkUser.DonutGetFriends(api.Params{
+		"owner_id": -173151748,
+	})
+	noError(t, err)
 }
 
 func TestVK_DonutGetSubscription(t *testing.T) {
-	// TODO: v5.125
+	t.Parallel()
+
+	needUserToken(t)
+
+	_, err := vkUser.DonutGetSubscription(api.Params{
+		"owner_id": -173151748,
+	})
+	if !errors.Is(err, api.ErrNotFound) {
+		noError(t, err)
+	}
 }
 
 func TestVK_DonutGetSubscriptions(t *testing.T) {
-	// TODO: v5.125
+	t.Parallel()
+
+	needUserToken(t)
+
+	_, err := vkUser.DonutGetSubscriptions(nil)
+	noError(t, err)
 }
 
 func TestVK_DonutIsDon(t *testing.T) {
-	// TODO: v5.125
+	t.Parallel()
+
+	needUserToken(t)
+
+	_, err := vkUser.DonutIsDon(api.Params{
+		"owner_id": -173151748,
+	})
+	noError(t, err)
 }
