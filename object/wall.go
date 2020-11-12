8ef07156f47f4c47a79733141c1851d4e3c98036
Suvorov Daniil <severecloud@gmail.com>
2020-11-12 14:12:39 +0300

    feat: WallWallpostToID add Donut

--- object/wall.go
+++ object/wall.go
@@ -216,25 +216,27 @@ type WallWallpostAttachment struct {
 
 // WallWallpostToID struct.
 type WallWallpostToID struct {
-	Attachments  []WallWallpostAttachment `json:"attachments"`
-	Comments     BaseCommentsInfo         `json:"comments"`
-	CopyOwnerID  int                      `json:"copy_owner_id"` // ID of the source post owner
-	CopyPostID   int                      `json:"copy_post_id"`  // ID of the source post
-	Date         int                      `json:"date"`          // Date of publishing in Unixtime
-	FromID       int                      `json:"from_id"`       // Post author ID
-	Geo          BaseGeo                  `json:"geo"`
-	ID           int                      `json:"id"` // Post ID
-	Likes        BaseLikesInfo            `json:"likes"`
-	PostID       int                      `json:"post_id"` // wall post ID (if comment)
-	PostSource   WallPostSource           `json:"post_source"`
-	PostType     string                   `json:"post_type"`
-	Reposts      BaseRepostsInfo          `json:"reposts"`
-	SignerID     int                      `json:"signer_id"`   // Post signer ID
-	Text         string                   `json:"text"`        // Post text
-	ToID         int                      `json:"to_id"`       // Wall owner's ID
-	IsFavorite   BaseBoolInt              `json:"is_favorite"` // Information whether the post in favorites list
-	MarkedAsAds  BaseBoolInt              `json:"marked_as_ads"`
-	ParentsStack []int                    `json:"parents_stack"`
+	Attachments   []WallWallpostAttachment `json:"attachments"`
+	Comments      BaseCommentsInfo         `json:"comments"`
+	CopyOwnerID   int                      `json:"copy_owner_id"` // ID of the source post owner
+	CopyPostID    int                      `json:"copy_post_id"`  // ID of the source post
+	Date          int                      `json:"date"`          // Date of publishing in Unixtime
+	FromID        int                      `json:"from_id"`       // Post author ID
+	Geo           BaseGeo                  `json:"geo"`
+	ID            int                      `json:"id"` // Post ID
+	Likes         BaseLikesInfo            `json:"likes"`
+	PostID        int                      `json:"post_id"` // wall post ID (if comment)
+	PostSource    WallPostSource           `json:"post_source"`
+	PostType      string                   `json:"post_type"`
+	Reposts       BaseRepostsInfo          `json:"reposts"`
+	SignerID      int                      `json:"signer_id"`   // Post signer ID
+	Text          string                   `json:"text"`        // Post text
+	ToID          int                      `json:"to_id"`       // Wall owner's ID
+	IsFavorite    BaseBoolInt              `json:"is_favorite"` // Information whether the post in favorites list
+	MarkedAsAds   BaseBoolInt              `json:"marked_as_ads"`
+	ParentsStack  []int                    `json:"parents_stack"`
+	Donut         WallWallpostDonut        `json:"donut"` // need api v5.125
+	ShortTextRate float64                  `json:"short_text_rate"`
 }
 
 // WallWallpostDonut info about VK Donut.
