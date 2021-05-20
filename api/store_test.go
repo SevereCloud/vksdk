f533e49f39528a2db6a80d9b8797bd286da3203a
Suvorov Daniil <severecloud@gmail.com>
2021-05-20 12:27:08 +0300

    feat: add StoreGetFavoriteStickers method

---
+++ api/store_test.go
@@ -0,0 +1,30 @@
+package api_test
+
+import (
+	"testing"
+
+	"github.com/SevereCloud/vksdk/v2/api"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVK_StoreGetFavoriteStickers(t *testing.T) {
+	t.Parallel()
+
+	needUserToken(t)
+
+	_, err := vkUser.StoreAddStickersToFavorite(api.Params{
+		"sticker_ids": 1,
+	})
+	noError(t, err)
+
+	res, err := vkUser.StoreGetFavoriteStickers(api.Params{})
+	noError(t, err)
+	assert.NotEmpty(t, res.Count)
+	assert.NotEmpty(t, res.Items)
+
+	_, err = vkUser.StoreRemoveStickersFromFavorite(api.Params{
+		"sticker_ids": 1,
+	})
+	noError(t, err)
+}
