2d8887360c6212059382371788dc05b292226795
Suvorov Daniil <severecloud@gmail.com>
2021-05-29 10:13:35 +0300

    fix: UsersPersonal crutch

--- object/users_test.go
+++ object/users_test.go
@@ -1,9 +1,11 @@
 package object_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/SevereCloud/vksdk/v2/object"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUsersUser_ToMention(t *testing.T) {
@@ -34,4 +36,23 @@ func TestUsersUserMin_ToMention(t *testing.T) {
 		object.UsersUserMin{ID: 1, FirstName: "Ivan", LastName: "Ivanov"},
 		"[id1|Ivan Ivanov]",
 	)
+}
+
+func TestUsersPersonal__UnmarshalJSON(t *testing.T) {
+	t.Parallel()
+
+	f := func(data []byte, wantpersonal object.UsersPersonal) {
+		var personal object.UsersPersonal
+
+		err := json.Unmarshal(data, &personal)
+		assert.NoError(t, err)
+		assert.Equal(t, wantpersonal, personal)
+	}
+
+	f([]byte("[]"), object.UsersPersonal{})
+	f([]byte(`{"alcohol":1}`), object.UsersPersonal{Alcohol: 1})
+
+	var personal object.UsersPersonal
+	err := json.Unmarshal([]byte("0"), &personal)
+	assert.Error(t, err)
 }
