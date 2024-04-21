40ba2bf0bd630f4a090c4fafad661955e54a2760
Dmitrii <4094695+Bercof@users.noreply.github.com>
2024-04-21 15:22:17 +0300

    fix: Set default empty filter for Groups.GetMembers

--- api/groups.go
+++ api/groups.go
@@ -390,7 +390,7 @@ type GroupsGetMembersResponse struct {
 //
 // https://dev.vk.com/method/groups.getMembers
 func (vk *VK) GroupsGetMembers(params Params) (response GroupsGetMembersResponse, err error) {
-	err = vk.RequestUnmarshal("groups.getMembers", &response, params, Params{"filter": ""})
+	err = vk.RequestUnmarshal("groups.getMembers", &response, Params{"filter": ""}, params)
 
 	return
 }
